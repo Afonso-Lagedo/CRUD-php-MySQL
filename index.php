255d45770e1f458f7340e5aee4c19a08bb7f58f7
Afonso-cmd <afonso.ur@gmail.com>
2021-01-23 12:31:26 -0300

    variáveis e operações

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-<?php echo 'Olá Mundo';
+<?php $mensagem = 'hello word';// declarando variável
+
+echo $mensagem;
+
+$A=2;//declarando variável
+$B=3;
+
+$soma = $A + $B; // operações    - * /
+print $soma//imprimindo na tela
+
+
+
+
+?>
