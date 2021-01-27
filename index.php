9bc81fb7e69d539038faffd27c1374487555e879
Afonso-cmd <afonso.ur@gmail.com>
2021-01-27 10:45:29 -0300

    alterando nome de tabela via php

--- index.php
+++ index.php
@@ -8,37 +8,6 @@ $database = "teste1";
 
 $conexao = mysqli_connect($servidor, $usuario, $senha, $database);
 	
-$consulta = mysqli_query($conexao, "SELECT * FROM ALUNOS");
-// PARA IMPRIMIR MYSQLI RESULT print_r($consulta);
-
-echo'
-	<table border=1>
-		<tr>
-			<th>id</th>
-			<th>Nome</th>
-			<th>Data de nascimento</th>
-		</tr>
-';
-
-while($linha = mysqli_fetch_array($consulta)){
-	echo '<tr>';
-
-	echo '<td>';
-	echo $linha['id_aluno'];
-	echo '</td>';
-
-	echo '<td>';
-	echo $linha['nome_aluno'];
-	echo '</td>';
-
-	echo '<td>';
-	echo $linha['data_nascimento'];
-	echo '</td>';
-
-	echo '</tr>';
-}
-
-echo '</table>';
-
+mysqli_query($conexao, "ALTER TABLE CURSOS CHANGE id id_curso INT NOT NULL AUTO_INCREMENT");
 ?>
 
