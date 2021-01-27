373799e7044e6ec9f545d51ee0969868e1684dcc
Afonso-cmd <afonso.ur@gmail.com>
2021-01-26 21:05:02 -0300

    consulta via php

--- index.php
+++ index.php
@@ -8,9 +8,37 @@ $database = "teste1";
 
 $conexao = mysqli_connect($servidor, $usuario, $senha, $database);
 	
-if (mysqli_query($conexao, "UPDATE ALUNOS SET NOME_ALUNO='FELIPE' WHERE ID_ALUNO =3 ")){
-	echo'Sucesso';
+$consulta = mysqli_query($conexao, "SELECT * FROM ALUNOS");
+// PARA IMPRIMIR MYSQLI RESULT print_r($consulta);
+
+echo'
+	<table border=1>
+		<tr>
+			<th>id</th>
+			<th>Nome</th>
+			<th>Data de nascimento</th>
+		</tr>
+';
+
+while($linha = mysqli_fetch_array($consulta)){
+	echo '<tr>';
+
+	echo '<td>';
+	echo $linha['id_aluno'];
+	echo '</td>';
+
+	echo '<td>';
+	echo $linha['nome_aluno'];
+	echo '</td>';
+
+	echo '<td>';
+	echo $linha['data_nascimento'];
+	echo '</td>';
+
+	echo '</tr>';
 }
+
+echo '</table>';
 
 ?>
 
