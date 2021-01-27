624f6238c1a414c8e58a1f6d3b17ce5ae08a9ad1
Afonso-cmd <afonso.ur@gmail.com>
2021-01-27 11:05:43 -0300

    consulta via php

--- index.php
+++ index.php
@@ -8,6 +8,27 @@ $database = "teste1";
 
 $conexao = mysqli_connect($servidor, $usuario, $senha, $database);
 	
-mysqli_query($conexao, "ALTER TABLE CURSOS CHANGE id id_curso INT NOT NULL AUTO_INCREMENT");
+$consulta = mysqli_query($conexao, "SELECT ALUNOS.nome_aluno, CURSOS.nome_curso FROM ALUNOS, CURSOS, ALUNOS_CURSOS WHERE ALUNOS_CURSOS.id_aluno = ALUNOS.id_aluno and ALUNOS_CURSOS.id_curso = CURSOS.id_curso");
+echo'<table border=1>
+		<tr> 
+			<th>Nome do aluno</th>  
+			<th>Nome do curso</th> 
+		</tr>';
+
+while ($linha = mysqli_fetch_array($consulta)){
+
+	echo'<tr> 
+			<td>'.
+				$linha['nome_aluno']
+			.'</td>
+			<td>'.
+				$linha['nome_curso']
+			.'</td>
+		</tr>';  
+}
+
+echo '</table>';
+
 ?>
+
 
