91de846cec5dca98c14636ddf44777709e8f00e4
Afonso-cmd <afonso.ur@gmail.com>
2021-02-03 18:38:25 -0300

    preparando CRUD

--- index.php
+++ index.php
@@ -1,34 +1,13 @@
 <?php 
 
-#mysqli
-$servidor = "localhost";
-$usuario = "root";
-$senha = "";
-$database = "teste1";
+#Base de dados
+include 'db.php';
 
-$conexao = mysqli_connect($servidor, $usuario, $senha, $database);
-	
-$consulta = mysqli_query($conexao, "SELECT ALUNOS.nome_aluno, CURSOS.nome_curso FROM ALUNOS, CURSOS, ALUNOS_CURSOS WHERE ALUNOS_CURSOS.id_aluno = ALUNOS.id_aluno and ALUNOS_CURSOS.id_curso = CURSOS.id_curso");
-echo'<table border=1>
-		<tr> 
-			<th>Nome do aluno</th>  
-			<th>Nome do curso</th> 
-		</tr>';
+#Cabeçalho
+include 'header.php';
 
-while ($linha = mysqli_fetch_array($consulta)){
+#Conteúdo da página
+include 'views/home.php';
 
-	echo'<tr> 
-			<td>'.
-				$linha['nome_aluno']
-			.'</td>
-			<td>'.
-				$linha['nome_curso']
-			.'</td>
-		</tr>';  
-}
-
-echo '</table>';
-
-?>
-
-
+#Rodapé
+include 'footer.php';
